32c0235773bfdb90673a59c38c2fee04e224034a
seth <seth.e.remer@gmail.com>
2023-06-06 11:48:10 -0600

    creating endpoints for login and signup

--- startup-html/database.js
+++ startup-html/database.js
@@ -1,10 +1,34 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./config.json');
 
 const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('ditty');
+const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
+
+function getUser(username) {
+    return userCollection.findOne({ username: username })
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function signUpUser(username, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        username: username,
+        password: passwordHash,
+        token: uuid.v4(),
+    }
+    await userCollection.insertOne(user);
+
+    return user;
+}
 
 async function addScore(score) {
     const result = await scoreCollection.insertOne(score);
@@ -31,5 +55,12 @@ function getScoreByTitle(username, title) {
     return cursor.toArray();
 }
 
-module.exports = { addScore, getRecentScores, getScoreByTitle };
+module.exports = { 
+    getUser,
+    getUserByToken,
+    signUpUser,
+    addScore, 
+    getRecentScores, 
+    getScoreByTitle,
+ };
 
--- startup-html/index.js
+++ startup-html/index.js
@@ -1,3 +1,5 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const axios = require('axios');
 const config = require('./config.json');
@@ -8,10 +10,55 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use(express.static('public'));
+
+app.set('trust proxy', true);
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/signup', async (req, res) => {
+  if (await DB.getUser(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.signUpUser(req.body.username, req.body.password);
+
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    })
+  }
+})
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+})
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+})
+
+apiRouter.get('/user/:username', async (req, res) => {
+  const user = await DB.getUser(req.params.username);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ username: user.username, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
 
 apiRouter.get('/lyrics', async (req, res) => {
     const songTitle = req.query.songTitle;
@@ -55,6 +102,14 @@ apiRouter.get('/scoresTitle', async (req, res) => {
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
   
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
