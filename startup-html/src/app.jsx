22f05bb92f99f55328b5413191baba66ea94991d
seth <seth.e.remer@gmail.com>
2023-06-13 11:10:33 -0600

    created browser router

--- startup-html/src/app.jsx
+++ startup-html/src/app.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Mode } from './mode/mode';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { Select } from './select/select';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= {
+          <Login />
+        }
+        exact
+      />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
